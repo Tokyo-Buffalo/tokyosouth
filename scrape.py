8d4eac066c1771f0c691698a29ffceeed5e70360
Kevin <kevinmoneil@me.com>
2017-01-08 03:49:17 +0900

    start using scrapy

--- scrape.py
+++ scrape.py
@@ -5,9 +5,12 @@ Scraping Southwest Airlines for Search Results
 @creation date: 1/5/2017 Year of the Trump
 """
 
-from lxml import html
 import requests
 import datetime
+import scrapy
+from lxml import html
+from scrapy.spiders import CrawlSpider, Rule
+#from scrapy.contrib.linkextractors import LinkExtractor
 
 dateNow = datetime.datetime.now()
 year = dateNow.year
@@ -17,6 +20,14 @@ day = dateNow.day
 page = requests.get("http://www.southwest.com")
 tree = html.fromstring(page.content)
 
+class Scrape(scrapy.Item):
+	originAirport = scrapy.Field()
+	destinationAirpot = scrapy.Field()
+
+class SouthwestSpider(CrawlSpider):
+	name = 'southwest'
+	allowed_domains = ['southwest.com']
+	start_url = 'https://www.southwest.com'
 
 def main():
 
