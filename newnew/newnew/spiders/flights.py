ce0ff92fb57d1acf24fcb62669eef6d17372906f
LarryBuff <larrybuffaloboy@gmail.com>
2017-01-26 12:06:51 -0500

    Used Form Request to grab the html results, now we just need to seperate the relevant data. Possibly clean up the files as well

--- newnew/newnew/spiders/flights.py
+++ newnew/newnew/spiders/flights.py
@@ -1,18 +1,33 @@
 import scrapy
+from scrapy.spider import BaseSpider
+from scrapy.http import FormRequest, Request
 
-class FlightsSpider(scrapy.Spider):
+class FlightsSpider(BaseSpider):
 	name = "flights"
 
 	def start_requests(self):
 		urls = [
-			'https://www.southwest.com',
+			'https://www.southwest.com/flight/search-flight.html?int=HOMEQBOMAIR',
 		]
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
-
+	
+	"""
 	def parse(self, response):
 		page = response.url.split("/")[-2]
 		filename = "flights-%s.html" % page
 		with open(filename, 'wb') as f:
 			f.write(response.body)
 			f.log('Saved file %s' % filename)
+	"""
+	
+	def parse(self, response):
+		yield FormRequest.from_response(response, formname='buildItineraryForm', formdata={'originAirport_displayed':'Washington (Dulles), DC - IAD', 'destinationAirport_displayed':'Ft. Lauderdale, FL - FLL', 'outboundDateString':'02/20/2017', 'returnDateString':'03/30/2017'}, callback=self.parseFlights)
+	
+	def parseFlights(self, response):
+		filename = "flights.html"
+		with open(filename, 'wb') as f:
+			f.write(response.body)
+		self.log('Saved file %s' % filename)
+
+	
