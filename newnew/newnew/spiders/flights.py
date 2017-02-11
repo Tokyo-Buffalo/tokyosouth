0fcbff35b6bfc9944b5196a6b059e4549d989c06
Kevin O'Neil <kevinmoneil@me.com>
2017-02-12 02:16:59 +0900

    cleanup

--- newnew/newnew/spiders/flights.py
+++ newnew/newnew/spiders/flights.py
@@ -9,19 +9,10 @@ class FlightsSpider(BaseSpider):
 
 	def start_requests(self):
 		urls = [
-			'https://www.southwest.com/flight/search-flight.html?int=HOMEQBOMAIR',
+			'https://www.southwest.com/flight/search-flight.html',
 		]
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
-
-	"""
-	def parse(self, response):
-		page = response.url.split("/")[-2]
-		filename = "flights-%s.html" % page
-		with open(filename, 'wb') as f:
-			f.write(response.body)
-			f.log('Saved file %s' % filename)
-	"""
 
 	def parse(self, response):
 		yield FormRequest.from_response(
