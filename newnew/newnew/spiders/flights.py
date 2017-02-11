cd239abbfe55cd23890a69fddef601602970fc04
Kevin O'Neil <kevinmoneil@me.com>
2017-02-12 01:12:39 +0900

    got scrapy to scrape form results

--- newnew/newnew/spiders/flights.py
+++ newnew/newnew/spiders/flights.py
@@ -11,7 +11,7 @@ class FlightsSpider(BaseSpider):
 		]
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
-	
+
 	"""
 	def parse(self, response):
 		page = response.url.split("/")[-2]
@@ -20,14 +20,24 @@ class FlightsSpider(BaseSpider):
 			f.write(response.body)
 			f.log('Saved file %s' % filename)
 	"""
-	
+
 	def parse(self, response):
-		yield FormRequest.from_response(response, formname='buildItineraryForm', formdata={'originAirport_displayed':'Washington (Dulles), DC - IAD', 'destinationAirport_displayed':'Ft. Lauderdale, FL - FLL', 'outboundDateString':'02/20/2017', 'returnDateString':'03/30/2017'}, callback=self.parseFlights)
-	
+		yield FormRequest.from_response(
+			response,
+			formname='buildItineraryForm',
+			formdata = {
+				'originAirport': 'IAD',
+				'originAirport_displayed': 'Washington (Dulles), DC - IAD',
+				'destinationAirport': 'FLL',
+				'destinationAirport_displayed': 'Ft. Lauderdale, FL - FLL',
+				'outboundDateString': '02/20/2017',
+				'returnDateString': '03/30/2017'
+			},
+			callback=self.parseFlights
+		)
+
 	def parseFlights(self, response):
 		filename = "flights.html"
 		with open(filename, 'wb') as f:
 			f.write(response.body)
 		self.log('Saved file %s' % filename)
-
-	
